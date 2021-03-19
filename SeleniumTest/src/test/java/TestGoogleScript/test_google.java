3af51f5b8e59e12fda0a9b44b223084be2d115ce
chetna-agrawal <chetna.agrawal3@gmail.com>
2021-03-19 17:06:39 +1100

    FirstAutomationProject

---
+++ SeleniumTest/src/test/java/TestGoogleScript/test_google.java
@@ -0,0 +1,12 @@
+package TestGoogleScript;
+
+import org.testng.annotations.Test;
+
+public class test_google {
+
+	@Test
+	public void testgoogle()
+	{
+		System.out.println("Testing Google Message");
+	}
+}
